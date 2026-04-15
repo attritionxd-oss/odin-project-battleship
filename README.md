a0b586a80a7e712a862b94c996bbbd0beaf035e7
attritionxd-oss <attritionxd@gmail.com>
2026-04-16 07:20:08 +0800

    build: init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project: Battleship
+
+This repository is a solution to the project outlined in [The Odin Project > Project: Battleship](https://www.theodinproject.com/lessons/node-path-javascript-battleship).
---
+++ jest.config.js
@@ -0,0 +1,15 @@
+// specifies files from triggering a test run
+export default {
+  watchPathIgnorePatterns: [
+    "<rootDir>/src/sandbox/sandbox*.js",
+    "<rootDir>/build/*.js",
+    "<rootDir>*.sandbox.js",
+    ".*\\.mjs$",
+  ],
+  // use the 'node' environment for service/API testing
+  testEnvironment: "node",
+  // autoclear mock calls and instances between tests
+  clearMocks: true,
+  // ensure Jest finds the tests directory
+  testMatch: ["<rootDir>/tests/**/*.test.js"],
+};
