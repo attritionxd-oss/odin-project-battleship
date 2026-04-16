0bd4234f737c51e00842812befedf8ff2f769077
attritionxd-oss <attritionxd@gmail.com>
2026-04-16 19:27:34 +0800

    build: eslint-jest configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,32 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import jest from "eslint-plugin-jest";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+      },
+    },
+  },
+  {
+    files: ["**/*.test.js", "**/*.spec.js"],
+    plugins: { jest },
+    rules: {
+      ...jest.configs.recommended.rules,
+      "jest/no-disabled-tests": "warn",
+    },
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+  },
 ]);
