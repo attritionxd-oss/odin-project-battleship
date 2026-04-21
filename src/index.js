daff3de50e0097fc92a121f6986fa3eb202bc915
attritionxd-oss <attritionxd@gmail.com>
2026-04-21 22:39:20 +0800

    build: index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-import "./style-vars.css";
-import "./style.css";
+import "./styles/style-vars.css";
+import "./styles/style.css";
+import "./styles/style-welcome-modal.css";
+import "./styles/style-game-setup-modal.css";
+import "./styles/style-gameboard.css";
+
+import App from "./controller/App.js";
+import GameEngine from "./controller/GameEngine.js";
+
+function bootstrap() {
+  const gameEngine = new GameEngine();
+  const app = new App(gameEngine);
+  app.initEventListeners();
+}
+
+bootstrap();
